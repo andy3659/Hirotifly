a67ec5ccd3bdcd12ea8dece4589ba038da7bb136
Andy Willy <andy3659@gmail.com>
2023-04-16 19:05:46 +0700

    Update saveAudioFile.ts

--- next-server/functions/fileSystem/saveAudioFile.ts
+++ next-server/functions/fileSystem/saveAudioFile.ts
@@ -12,6 +12,7 @@ const saveAudioFile = ({
   const audioPath = path.join(process.cwd(), process.env.AUDIO_BASE_PATH!);
   const filePath = audioPath + videoId + ".m4a";
   const file = createWriteStream(filePath);
-  return stream.pipe(file);
+  stream.pipe(file);
+  return file;
 };
 export { saveAudioFile };
